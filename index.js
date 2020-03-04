d9fd86e31ef97319f751868851d6afadec318d5e
github username <github email address>
2020-03-04 16:58:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(array, funct){
+  let result = []
+  array.forEach(num => result.push(funct(num)))
+  return result
+}
+
+function reduce(array, funct, startingPoint){
+  let start = startingPoint || array[0]
+  array.forEach(elem => (start = funct(elem, start)))
+  return typeof start == 'number'
+  ? !!startingPoint
+  ? start
+  : start - array[0]
+  : start
+}
